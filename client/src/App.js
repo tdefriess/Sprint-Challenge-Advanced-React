67483f09831da0f7cafe82addd4f97de8758725a
Tyler de Friess <tyler.defriess@gmail.com>
2020-02-14 11:14:36 -0500

    Initial commit, refactored App as class component, axios call working, not set to state yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount(){
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log('response', res);
+      })
+      .catch(err => console.log('The data was not returned', err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }  
 }
 
 export default App;
