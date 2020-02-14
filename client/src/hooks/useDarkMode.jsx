c3e686edb32bdc15ecb1630599bedfe7436474b4
Tyler de Friess <tyler.defriess@gmail.com>
2020-02-14 12:22:54 -0500

    Added local storage hook, refactored dark mode hook to connect to local storage hook

--- client/src/hooks/useDarkMode.jsx
+++ client/src/hooks/useDarkMode.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
-import { useState } from 'react';
+import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = () => {
-    const [useDark, setUseDark] = useState(false);
+export const useDarkMode = (initial) => {
+    const [useDark, setUseDark] = useLocalStorage('darkMode', initial);
 
     useEffect(() => {
         useDark ? document.querySelector('body').classList.add('dark-mode')
-        : document.querySelector('body').classList.remove('dark-mode')        
+        : document.querySelector('body').classList.remove('dark-mode');
+
     }, [useDark])
 
     return [useDark, setUseDark];
